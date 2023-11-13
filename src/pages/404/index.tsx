4178e175a582eb72a8e59a4d2408b2b123fffc5b
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-14 01:21:03 +0700

    update

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,7 +3,6 @@ import { useRouteError, Link } from "react-router-dom";
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
-
     return (
         <>
             <section className="d-flex flex-column align-items-center justify-content-center cx_bg-drawing postion-relative w-100 h-100">
