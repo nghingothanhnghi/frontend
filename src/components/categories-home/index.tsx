3704d04ca4c167e2bfd04edca319e1614b249167
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-13 22:47:45 +0700

    update

--- src/components/categories-home/index.tsx
+++ src/components/categories-home/index.tsx
@@ -67,9 +67,9 @@ export default class CategoriesList extends Component<Props, State>{
     });
   }
 
-  setActiveTutorial(tutorial: ICategoryData, index: number) {
+  setActiveTutorial(post: ICategoryData, index: number) {
     this.setState({
-      currentTutorial: tutorial,
+      currentTutorial: post,
       currentIndex: index
     });
   }
@@ -133,16 +133,25 @@ export default class CategoriesList extends Component<Props, State>{
 
           <ul className="list-group">
             {categories &&
-              categories.map((tutorial: ICategoryData, index: number) => (
+              categories.map((post: ICategoryData, index: number) => (
                 <li
                   className={
                     "list-group-item " +
                     (index === currentIndex ? "active" : "")
                   }
-                  onClick={() => this.setActiveTutorial(tutorial, index)}
+                  onClick={() => this.setActiveTutorial(post, index)}
                   key={index}
                 >
-                  {tutorial.title}
+                                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {post.title}</h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                </a>
+            </div>
+                 
                 </li>
               ))}
           </ul>
