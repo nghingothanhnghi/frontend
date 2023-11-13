d57071ed2481642ca101fb301b0480fec5d5fec4
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-13 22:31:40 +0700

    update

--- src/components/categories-home/index.tsx
+++ src/components/categories-home/index.tsx
@@ -1,18 +1,198 @@
 
 
-export default function CategoriesHomeComponent() {
+
+
+import { Component, ChangeEvent } from "react";
+import CategoryDataService from "../../services/category.service";
+import { Link } from "react-router-dom";
+import ICategoryData from "../../models/category.model";
+
+type Props = {};
+
+type State = {
+  categories: Array<ICategoryData>,
+  currentTutorial: ICategoryData | null,
+  currentIndex: number,
+  searchTitle: string
+};
+
+export default class CategoriesList extends Component<Props, State>{
+  constructor(props: Props) {
+    super(props);
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveCategories = this.retrieveCategories.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveTutorial = this.setActiveTutorial.bind(this);
+    this.removeAllTutorials = this.removeAllTutorials.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
+
+    this.state = {
+      categories: [],
+      currentTutorial: null,
+      currentIndex: -1,
+      searchTitle: ""
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveCategories();
+  }
+
+  onChangeSearchTitle(e: ChangeEvent<HTMLInputElement>) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle
+    });
+  }
+
+  retrieveCategories() {
+    CategoryDataService.getAll()
+      .then((response: any) => {
+        this.setState({
+          categories: response.data
+        });
+        console.log(response.data);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveCategories();
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+  }
+
+  setActiveTutorial(tutorial: ICategoryData, index: number) {
+    this.setState({
+      currentTutorial: tutorial,
+      currentIndex: index
+    });
+  }
+
+  removeAllTutorials() {
+    CategoryDataService.deleteAll()
+      .then((response: any) => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+
+  searchTitle() {
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+
+    CategoryDataService.findByTitle(this.state.searchTitle)
+      .then((response: any) => {
+        this.setState({
+          categories: response.data
+        });
+        console.log(response.data);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { searchTitle, categories, currentTutorial, currentIndex } = this.state;
+
     return (
-        <>
-            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                </a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                </a>
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={this.onChangeSearchTitle}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.searchTitle}
+              >
+                Search
+              </button>
             </div>
-        </>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Tutorials List</h4>
+
+          <ul className="list-group">
+            {categories &&
+              categories.map((tutorial: ICategoryData, index: number) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveTutorial(tutorial, index)}
+                  key={index}
+                >
+                  {tutorial.title}
+                </li>
+              ))}
+          </ul>
+
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllTutorials}
+          >
+            Remove All
+          </button>
+        </div>
+        <div className="col-md-6">
+          {currentTutorial ? (
+            <div>
+              <h4>Tutorial</h4>
+              <div>
+                <label>
+                  <strong>Title:</strong>
+                </label>{" "}
+                {currentTutorial.title}
+              </div>
+              <div>
+                <label>
+                  <strong>Description:</strong>
+                </label>{" "}
+                {currentTutorial.description}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentTutorial.published ? "Published" : "Pending"}
+              </div>
+
+              <Link
+                to={"/categories/" + currentTutorial.id}
+                className="badge badge-warning"
+              >
+                Edit
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on a Tutorial...</p>
+            </div>
+          )}
+        </div>
+      </div>
     );
+  }
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import HeroComponent from '../../components/hero'
+import CategoriesList from '../../components/categories-home'
 
 function Home() {
 
   return (
     <>
       <HeroComponent></HeroComponent>
+      <CategoriesList></CategoriesList>
     </>
   )
 }
