271f47e8da2397ed085d60fb649f7bb6058fc3dc
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-13 19:26:36 +0700

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import { Disclosure } from '@headlessui/react'
-import { ChevronUpIcon } from '@heroicons/react/20/solid'
 import HeroComponent from '../../components/hero'
 
 function Home() {
