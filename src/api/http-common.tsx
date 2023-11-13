82d313f4a1d105e4b7b2371fea8d99b94949b8e1
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-13 22:39:56 +0700

    update

--- src/api/http-common.tsx
+++ src/api/http-common.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+
+const BASE_URL = 'http://api.chuotgreen.com/api/';
+
 export default axios.create({
-  baseURL: "http://api.chuotgreen.com/api/",
-  headers: {
+    baseURL: BASE_URL,
+    headers: {
     "Content-type": "application/json"
   }
 });
