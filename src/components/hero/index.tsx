ab3cc5121b5b726d46b747a978fdbb5c1bea45ac
Nghi Ngo <nghingothanhnghi@gmail.com>
2023-11-13 23:38:20 +0700

    update

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,9 +5,9 @@ export default function HeroComponent() {
         <>
             <section className="bg-white dark:bg-gray-900">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
-                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-                    <SearchBar/>
+                    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Welcome to the Help Center </p>
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">How can we help?</h1>
+                    <SearchBar />
                 </div>
             </section>
         </>
